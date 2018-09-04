f2bc6754447b3bd296198c02acf3f78f9e56a86b
feru-ali <feru.ali@gmail.com>
2018-09-03 22:42:25 -0700

    Update glibc_installer.sh

--- glibc_installer.sh
+++ glibc_installer.sh
@@ -4,15 +4,12 @@
 
 wget ftp.gnu.org/gnu/glibc/glibc-2.28.tar.gz
 tar -xvfz glibc-2.28.tar.gz 	#extract the gzip file  
-
 cd glibc-2.28
 mkdir build
 cd build
-
 ../configure --prefix=/opt/glibc-2.28
 make
 sudo make install
 export LD_LIBRARY_PATH=/opt/glibc-2.28/lib:$LD_LIBRARY_PATH
 
-
-ldd --version #to verify glibc version
+ldd --version                 #to verify glibc version
