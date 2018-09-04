e3fc1fe5a42fbb0b2843f61cfaf2283aa20b2015
feru-ali <feru.ali@gmail.com>
2018-09-03 22:41:33 -0700

    script file added

---
+++ glibc_installer.sh
@@ -0,0 +1,18 @@
+##package for specific release can be downloaded from 
+
+###https://ftp.gnu.org/gnu/glibc/
+
+wget ftp.gnu.org/gnu/glibc/glibc-2.28.tar.gz
+tar -xvfz glibc-2.28.tar.gz 	#extract the gzip file  
+
+cd glibc-2.28
+mkdir build
+cd build
+
+../configure --prefix=/opt/glibc-2.28
+make
+sudo make install
+export LD_LIBRARY_PATH=/opt/glibc-2.28/lib:$LD_LIBRARY_PATH
+
+
+ldd --version #to verify glibc version
